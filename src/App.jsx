5e23bc487b1e22da596dd594456775ca38762897
Rebekah Callari-Kaczmarczyk <rebekahcallari@gmail.com>
2023-12-03 17:28:13 -0500

    add the data transformation

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import { useMemo } from "react";
 import Highcharts from "highcharts";
 import HighchartsReact from "highcharts-react-official";
 import { cityTemperature } from "@visx/mock-data";
@@ -23,20 +24,52 @@ import "./App.css";
 // 5. Create buttons that allow you to add/remove cities from the charts
 
 // Note: be careful because Highcharts mutates your data in place: https://github.com/highcharts/highcharts-react/issues/326
+
+// This is based off of the keys in `cityTemperature`
+// Using this format limits us to only one hard coded value that we can easily change if the data changes
+const CITY_KEYS = ["Austin", "San Francisco", "New York"];
+
 function App() {
   // View the data we'll be working with
-  console.log(cityTemperature);
+  // console.log(cityTemperature);
 
-  const cityTemperatureObject = [
-    {
-      cityName: "",
-      data: [
-        ["xValue1", "yValue1"],
-        ["xValue2", "yValue2"],
-      ],
-      mean: 0,
-    },
-  ];
+  // Transform and aggregate the data so that we can easily pass it to HighCharts
+  const parsedCityTemperatureData = useMemo(() => {
+    // This is our desired data structure
+    const cityArray = CITY_KEYS.map((city) => {
+      return { cityName: city, data: [], sumTemperature: 0, mean: null };
+    });
+    // Aggregate the data in a way that we can plot it
+    const transformedCityTemperatureData = cityTemperature.reduce(
+      (parsedData, currentValue) => {
+        // Get the date of the current value
+        const date = currentValue["date"];
+        // For each city, add the current data
+        parsedData.forEach((cityObject) => {
+          const city = cityObject.cityName;
+          // Get the temp for the current city
+          const temp = parseFloat(currentValue[city]);
+          // Highchats wants data in the format [[x1,y1], [x2, y2]]
+          cityObject.data.push([date, temp]);
+          // Add the temp to the running total (will be used to calculate average/mean)
+          cityObject.sumTemperature = cityObject.sumTemperature + temp;
+        });
+
+        return parsedData;
+      },
+      cityArray
+    );
+    // Calculate the mean temp for each city
+    transformedCityTemperatureData.forEach((cityObject) => {
+      cityObject.mean = cityObject.sumTemperature / cityObject.data.length;
+    });
+    return transformedCityTemperatureData;
+
+    // Hint: you could add city filter here
+  }, []);
+
+  console.log(parsedCityTemperatureData);
+
   const options = {
     title: {
       text: "My chart",
